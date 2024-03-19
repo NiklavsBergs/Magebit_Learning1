6803189eb8da8c147f646783f54c7c5f72ec25c8
Niklavs Bergs <niklavs.bergs@magebit.com>
2024-03-19 12:17:58 +0200

    Added Docs

--- app/code/MageMastery/FirstLayout/ViewModel/User.php
+++ app/code/MageMastery/FirstLayout/ViewModel/User.php
@@ -1,14 +1,31 @@
 <?php
+/**
+ * This file is part of the Magebit package.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magebit Faq
+ * to newer versions in the future.
+ *
+ * @copyright Copyright (c) 2024 Magebit, Ltd. (https://magebit.com/)
+ * @license   GNU General Public License ("GPL") v3.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace MageMastery\FirstLayout\ViewModel;
 
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
-Class User implements ArgumentInterface
+class User implements ArgumentInterface
 {
-    Private String $name = 'Friend';
+    private String $name = 'Friend';
 
-    public function getFirstName():String {
+    public function getFirstName():String
+    {
         return $this->name;
     }
 }
