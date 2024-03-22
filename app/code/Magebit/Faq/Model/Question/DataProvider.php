e2d78916643ddcfe32d9bea5a3b3850ac45a513a
Niklavs Bergs <niklavs.bergs@magebit.com>
2024-03-22 22:28:19 +0200

    Added edit function

--- app/code/Magebit/Faq/Model/Question/DataProvider.php
+++ app/code/Magebit/Faq/Model/Question/DataProvider.php
@@ -1,8 +1,10 @@
 <?php
 namespace Magebit\Faq\Model\Question;
 
-use Magento\Ui\DataProvider\AbstractDataProvider;
-use Magento\Ui\DataProvider\Modifier\ModifierInterface;
+use Magebit\Faq\Model\Question;
+use Magebit\Faq\Model\QuestionFactory;
+use Magebit\Faq\Model\ResourceModel\Question as QuestionResource;
+use Magento\Framework\App\RequestInterface;
 use Magento\Ui\DataProvider\Modifier\PoolInterface;
 use Magebit\Faq\Model\ResourceModel\Question\CollectionFactory;
 use Magento\Ui\DataProvider\ModifierPoolDataProvider;
@@ -10,18 +12,50 @@ use Magento\Ui\DataProvider\ModifierPoolDataProvider;
 class DataProvider extends ModifierPoolDataProvider
 {
 
-
+    private array $loadedData;
     public function __construct(
         $name,
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $CollectionFactory,
+        private QuestionResource $resource,
+        private QuestionFactory $questionFactory,
+        private RequestInterface $request,
         array $meta = [],
         array $data = [],
         PoolInterface $pool = null
     ) {
         $this->collection = $CollectionFactory->create();
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data, $pool);
+    }
+
+    public function getData(): array
+    {
+        if (isset($this->loadedData)){
+            return $this->loadedData;
+        }
+        $post = $this->getCurrentQuestion();
+        $this->loadedData[$post->getId()] = $post->getData();
+
+        return $this->loadedData;
+    }
+
+    private function getCurrentQuestion(): Question
+    {
+        $qId = $this->getQuestionId();
+        $question = $this->questionFactory->create();
+        if(!$qId){
+            return $question;
+        }
+
+        $this->resource->load($question, $qId);
+
+        return $question;
+    }
+
+    private function getQuestionId(): int
+    {
+        return (int) $this->request->getParam($this->getRequestFieldName());
     }
 
 }
--- app/code/Magebit/Faq/Ui/Component/Listing/Column/Actions.php
+++ app/code/Magebit/Faq/Ui/Component/Listing/Column/Actions.php
@@ -6,6 +6,7 @@ use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
 use Magebit\Faq\Block\Adminhtml\Module\Grid\Renderer\Action\UrlBuilder;
 use Magento\Framework\UrlInterface;
+use function _PHPStan_6b522806f\React\Async\parallel;
 
 class Actions extends Column {
     /** Url path */
@@ -14,39 +15,41 @@ class Actions extends Column {
     /** Url path */
     const URL_PATH_DELETE = 'faq/question/delete';
 
-    /** @var UrlBuilder */
-    protected $actionUrlBuilder;
-    /** @var UrlInterface */
-    protected $urlBuilder;
-    /**
-     * @param ContextInterface $context
-     * @param UiComponentFactory $uiComponentFactory
-     * @param UrlBuilder $actionUrlBuilder
-     * @param UrlInterface $urlBuilder
-     * @param array $components
-     * @param array $data
-     */
-    public function __construct(ContextInterface $context, UiComponentFactory $uiComponentFactory, UrlBuilder $actionUrlBuilder, UrlInterface $urlBuilder, array $components = [], array $data = []) {
-        $this->urlBuilder = $urlBuilder;
-        $this->actionUrlBuilder = $actionUrlBuilder;
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        private UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    )
+    {
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
+
     /**
      * Prepare Data Source
      *
      * @param array $dataSource
      * @return array
      */
-    public function prepareDataSource(array $dataSource) {
+    public function prepareDataSource(array $dataSource): array
+    {
         if (isset($dataSource['data']['items'])) {
-            foreach ($dataSource['data']['items'] as & $item) {
-                $name = $this->getData('name');
+            foreach ($dataSource['data']['items'] as &$item) {
                 if (isset($item['id'])) {
-                    $item[$name]['edit'] = ['href' => $this->urlBuilder->getUrl(self::URL_PATH_EDIT, ['id' => $item['id']]), 'label' => __('Edit') ];
+                    $name = $this->getData('name');
+                    $item[$name]['edit'] = [
+                        'href' => $this->getEditUrl($item),
+                        'label' => __('Edit') ];
                     $item[$name]['delete'] = ['href' => $this->urlBuilder->getUrl(self::URL_PATH_DELETE, ['id' => $item['id']]), 'label' => __('Delete') ];
                 }
             }
         }
         return $dataSource;
+    }
+
+    private function getEditUrl(array $item): string
+    {
+        return $this->urlBuilder->getUrl(self::URL_PATH_EDIT, ['id' => $item['id']]);
     }
 }
