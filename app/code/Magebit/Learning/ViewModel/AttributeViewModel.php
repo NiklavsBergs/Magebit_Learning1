56bd351665ce9a668843e337e69720a868fd6f7c
Niklavs Bergs <niklavs.bergs@magebit.com>
2024-03-24 22:49:48 +0200

    Displaying needed attributes

--- app/code/Magebit/Learning/ViewModel/AttributeViewModel.php
+++ app/code/Magebit/Learning/ViewModel/AttributeViewModel.php
@@ -19,29 +19,69 @@ declare(strict_types=1);
 namespace Magebit\Learning\ViewModel;
 
 use Exception;
+use Magento\Catalog\Model\ProductRepository;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-use Magento\Catalog\Block\Product\View\Attributes;
+use Magento\Catalog\Block\Product\View;
 
 class AttributeViewModel implements ArgumentInterface
 {
+    private string $ATTR_1 = 'dimensions';
+    private string $ATTR_2 = 'color';
+    private string $ATTR_3 = 'material';
+
+    public function __construct(private ProductRepository $productRepository)
+    {}
+
+    public function getAttributes(View $block)
+    {
+        $product = $block->getProduct();
+        return $product->getAttributes();
+    }
+
     /**
-     * @param Attributes $block
+     * @param View $block
      * @return array
      */
-    public function getAdditionalAttributes(Attributes $block): array
+    public function getAdditionalAttributes(View $block): array
     {
         try {
-            return $block->getAdditionalData();
+            $additionalAttributes = [];
+
+            $product = $block->getProduct();
+            $attributes = $product->getAttributes();
+
+            foreach($attributes as $attribute){
+                if($attribute->getAttributeCode() == $this->ATTR_1){
+                    $additionalAttributes[] = [
+                        'label' => $attribute->getDefaultFrontendLabel(),
+                        'value' => $attribute->getFrontend()->getValue($product)
+                        ];
+                }
+                if($attribute->getAttributeCode() == $this->ATTR_2){
+                    $additionalAttributes[] = [
+                        'label' => $attribute->getDefaultFrontendLabel(),
+                        'value' => $attribute->getFrontend()->getValue($product)
+                    ];
+                }
+                if($attribute->getAttributeCode() == $this->ATTR_3){
+                    $additionalAttributes[] = [
+                        'label' => $attribute->getDefaultFrontendLabel(),
+                        'value' => $attribute->getFrontend()->getValue($product)
+                    ];
+                }
+            }
+
+            return $additionalAttributes;
         } catch (Exception $e) {
             return [];
         }
     }
 
     /**
-     * @param Attributes $block
+     * @param View $block
      * @return string
      */
-    public function getShortDescription(Attributes $block): string
+    public function getShortDescription(View $block): string
     {
         $product = $block->getProduct();
 
